f8b52f37326e9ab926c4eeb8fab83ad2966ef8d2
Olivia <odecharette@gmail.com>
2016-05-29 10:35:16 +0200

    HomePage : ajout filtre sur les news WS

--- src/Becowo/CoreBundle/Controller/HomeController.php
+++ src/Becowo/CoreBundle/Controller/HomeController.php
@@ -17,20 +17,22 @@ class HomeController extends Controller
   	$repo = $em->getRepository('BecowoCoreBundle:Workspace');
   	$workspaces = $repo->findActiveWorkspaces();
 
-  	// Les x derniers membres inscrits et actifs
-  	$repo = $em->getRepository('BecowoCoreBundle:Member');
-  	$members = $repo->findNewMembers(5);
-
-  	//TO DO : récupérer que les 3 derniers espaces inscrits
+  	// Les x derniers WS crées et actifs
+  	$newWorkspaces = $repo->findNewWorkspaces(3);
 
 	// WS coup de coeur  	
   	$repo = $em->getRepository('BecowoCoreBundle:WorkspaceFavorite');
   	$workspaceFavorite = $repo->findOneBy(array(), array('createdOn' => 'desc'));
 
+  	// Les x derniers membres inscrits et actifs
+  	$repo = $em->getRepository('BecowoCoreBundle:Member');
+  	$members = $repo->findNewMembers(5);
+
   	return $this->render('BecowoCoreBundle:Home:home.html.twig', array(
   		'workspaces' => $workspaces, 
   		'members' => $members, 
-  		'workspaceFavorite' => $workspaceFavorite));
+  		'workspaceFavorite' => $workspaceFavorite,
+  		'newWorkspaces' => $newWorkspaces));
   }
 
 
--- src/Becowo/CoreBundle/Repository/WorkspaceRepository.php
+++ src/Becowo/CoreBundle/Repository/WorkspaceRepository.php
@@ -14,11 +14,32 @@ class WorkspaceRepository extends EntityRepository
 	{
 		// Récupère tous les workspace en BDD non supprimés et visibles
 		$qb = $this->createQueryBuilder('w');
-		$qb->where('w.isDeleted = false')
-			->andWhere('w.isVisible = true');
+
+		$this->whereIsActive($qb);
 
 		return $qb->getQuery()->getResult();
 	}
 
-	
+	public function findNewWorkspaces($nb)
+	{
+		// Récupère uniquement les X nouveaux WS actifs
+		$qb = $this->createQueryBuilder('w');
+
+		$this->whereIsActive($qb);
+
+		$qb->orderBy('w.createdOn', 'DESC')
+			->setFirstResult(0)
+			->setMaxResults($nb);
+
+		return $qb->getQuery()->getResult();
+	}
+
+	public function whereIsActive(QueryBuilder $qb)
+	{
+		// filtre sur les WS actifs uniquement
+		$qb->andWhere('w.isDeleted = false')
+			->andWhere('w.isVisible = true');
+	}
+
+
 }
