339d82e06783f840dbf1ba0e40b06393639dc0eb
Olivia <odecharette@gmail.com>
2016-06-13 12:59:31 +0200

    refacto homecontroller

--- src/Becowo/CoreBundle/Controller/HomeController.php
+++ src/Becowo/CoreBundle/Controller/HomeController.php
@@ -17,34 +17,36 @@ class HomeController extends Controller
     // Les x derniers WS crées et actifs
     $newWorkspaces = $repo->findNewWorkspaces(3);
 
+    //On récupère la picture favorite liée à chaque WS
+    $repo = $em->getRepository('BecowoCoreBundle:Picture');
     foreach ($newWorkspaces as $ws) {
-      //On récupère la picture favorite liée à chaque WS
-      $repo = $em->getRepository('BecowoCoreBundle:Picture');
       $picture = $repo->findByWsFavorite($ws->getName());
-
       $pictureFavoriteByWs[$ws->getName()] = $picture;
     }
 
+    //On récupère les pictures liées à chaque WS
     $picturesByWs = array();
+    //$repo = $em->getRepository('BecowoCoreBundle:Picture');
     foreach ($workspaces as $ws) {
-      //On récupère les pictures liées à chaque WS
-      $repo = $em->getRepository('BecowoCoreBundle:Picture');
       $pictures = $repo->findByWsNoLogo($ws->getName());
-
       $picturesByWs[$ws->getName()] = $pictures;
     }
 
-	// WS coup de coeur  	
+    // On récupère les offices et leur quantité
+    $officesByWS = array();
+    $repo = $em->getRepository('BecowoCoreBundle:WorkspaceHasOffice');
+    foreach ($workspaces as $ws) {
+      $offices = $repo->findBy(array('workspace' => $ws));
+      $officesByWS[$ws->getName()] = $offices;
+    }
+
+	  // WS coup de coeur  	
   	$repo = $em->getRepository('BecowoCoreBundle:WorkspaceFavorite');
   	$workspaceFavorite = $repo->findOneBy(array(), array('createdOn' => 'desc'));
 
   	// Les x derniers membres inscrits et actifs
   	$repo = $em->getRepository('BecowoMemberBundle:Member');
   	$members = $repo->findNewMembers(5);
-
-    // On récupère les offices et leur quantité
-    $repo = $em->getRepository('BecowoCoreBundle:WorkspaceHasOffice');
-    $listOffices = $repo->findBy(array('workspace' => $ws));
 
   	return $this->render('Home/home.html.twig', array(
   		'workspaces' => $workspaces, 
@@ -53,7 +55,7 @@ class HomeController extends Controller
   		'newWorkspaces' => $newWorkspaces,
       'picturesByWs' => $picturesByWs,
       'pictureFavoriteByWs' => $pictureFavoriteByWs,
-      'listOffices' => $listOffices));
+      'officesByWS' => $officesByWS));
   }
 
 }
