df8ce0d722d70173db4598406e15c3c3faa2c9b0
Olivia <odecharette@gmail.com>
2016-06-16 14:33:03 +0200

    marker from geojson récup en doctrine

--- src/Becowo/CoreBundle/Controller/TestController.php
+++ src/Becowo/CoreBundle/Controller/TestController.php
@@ -3,15 +3,42 @@
 namespace Becowo\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class TestController extends Controller
 {
   public function testAction()
   {
+    $em = $this->getDoctrine()->getManager();
+
+    // Tous les workspaces
+    $repo = $em->getRepository('BecowoCoreBundle:Workspace');
+    $workspaces = $repo->findActiveWorkspaces();
+
+    $geojson = array( 'type' => 'FeatureCollection', 'features' => array());
+
+    foreach ($workspaces as $w) {
+      $feature = array(
+        'id' => $w->getId(),
+        'type' => 'Feature', 
+        'geometry' => array(
+            'type' => 'Point',
+            # Pass Longitude and Latitude Columns here
+            'coordinates' => array($w->getLongitude(), $w->getLatitude())
+        ),
+        # Pass other attribute columns here
+        'properties' => array(
+            'address' => $w->getStreet(),
+            'city' => $w->getCity()
+            )
+        );
+    # Add feature arrays to feature collection array
+    array_push($geojson['features'], $feature);
+    }
+
+ $workspacesInJson = json_encode($geojson);
   	
-  	return $this->render('Test/test.html.twig');
+  	return $this->render('Test/test.html.twig', array('workspacesInJson' => $workspacesInJson, 'geojson' => $geojson));
   }
-
-
 
 }
