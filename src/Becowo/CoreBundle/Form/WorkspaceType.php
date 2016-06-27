1135ebace1a588e021351f8aeaf84fcb35f30d63
Olivia <odecharette@gmail.com>
2016-06-27 17:41:22 +0200

    form WS un Btn edit/read

---
+++ src/Becowo/CoreBundle/Form/WorkspaceType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Becowo\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class WorkspaceType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            // ->add('name')
+            ->add('description',   TextareaType::class)
+            // ->add('descriptionBonus',   TextType::class)
+            // ->add('website')
+            // ->add('isAlwaysOpen')
+            // ->add('street')
+            // ->add('postCode')
+            // ->add('city')
+            // ->add('longitude')
+            // ->add('latitude')
+            // ->add('firstBookingFree')
+            // ->add('facebookLink')
+            // ->add('twitterLink')
+            // ->add('instagramLink')
+            // ->add('isDeleted')
+            // ->add('createdOn', 'datetime')
+            // ->add('updatedOn', 'datetime')
+            // ->add('isVisible')
+            // ->add('category')
+            // ->add('country')
+            // ->add('poi')
+            // ->add('teamMember')
+            // ->add('amenities')
+            // ->add('offer')
+            // ->add('save',      SubmitType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Becowo\CoreBundle\Entity\Workspace'
+        ));
+    }
+}
--- src/Becowo/ManagerBundle/Controller/ProfileController.php
+++ src/Becowo/ManagerBundle/Controller/ProfileController.php
@@ -3,15 +3,29 @@
 namespace Becowo\ManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Becowo\CoreBundle\Entity\Workspace;
+use Becowo\CoreBundle\Form\WorkspaceType;
 
 class ProfileController extends Controller
 {
-  public function viewAction()
+  public function viewAction(Request $request)
   {
 
-  	//$ws = $this->getUser()->getWorkspace();
+  	$workspace = $this->getUser()->getWorkspace();	// current WS of connected manager
+  	$form = $this->get('form.factory')->create(WorkspaceType::class, $workspace);
 
-  	return $this->render('Manager/workspace_profile.html.twig');
+  	if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($workspace);
+      $em->flush();
+
+      $request->getSession()->getFlashBag()->add('success', 'Modification bien enregistrée.');
+
+      return $this->redirectToRoute('becowo_manager_profile');
+    }
+
+  	return $this->render('Manager/workspace_profile.html.twig', array('form' => $form->createView()));
   }
 
 
