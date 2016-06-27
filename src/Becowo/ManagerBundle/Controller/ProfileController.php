9b1f218dfc209964d8fa2b480d24c9e1206dde6f
Olivia <odecharette@gmail.com>
2016-06-27 12:23:43 +0200

    récup WS managé (1 seul pour le moment)

--- src/Becowo/ManagerBundle/Controller/ProfileController.php
+++ src/Becowo/ManagerBundle/Controller/ProfileController.php
@@ -8,11 +8,10 @@ class ProfileController extends Controller
 {
   public function viewAction()
   {
-  	$WsService = $this->get('app.workspace');
 
-  	$u = $this->getUser()->getWorkspace();
-    //$ws = $WsService->getWorkspaceByName($name);
-  	return $this->render('Manager/workspace_profile.html.twig', array('u' => $u));
+  	$managed_WS = $this->getUser()->getWorkspace();
+  	
+  	return $this->render('Manager/workspace_profile.html.twig', array('managed_WS' => $managed_WS));
   }
 
 
