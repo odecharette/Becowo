3f4872bf49ea8ac9347b5edec0c6b8a067ab5df9
Olivia <odecharette@gmail.com>
2016-07-04 18:54:50 +0200

    debut récap résa

--- web/js/book.js
+++ web/js/book.js
@@ -4,11 +4,11 @@
     document.getElementById("dateSemaine").style.display = 'none';
     document.getElementById("dateMois").style.display = 'none';
 
-	function goBooking($officeName)
+	function goBooking(officeName)
     {
-        document.getElementById("booking_titre").innerHTML = 'Réservation pour ' + $officeName;
+        document.getElementById("officeSelected").innerHTML = officeName;
     }        
-        
+       
     function radioClick(radioDuree){
         
         switch(radioDuree.value)
@@ -19,6 +19,7 @@
                 document.getElementById("dateJournee").style.display = 'none';
                 document.getElementById("dateSemaine").style.display = 'none';
                 document.getElementById("dateMois").style.display = 'none';
+                document.getElementById("durationSelected").innerHTML = "Heure";
                 break;
             case "DemiJournee":
                 document.getElementById("dateHeure").style.display = 'none';
@@ -26,6 +27,7 @@
                 document.getElementById("dateJournee").style.display = 'none';
                 document.getElementById("dateSemaine").style.display = 'none';
                 document.getElementById("dateMois").style.display = 'none';
+                document.getElementById("durationSelected").innerHTML = "Demi Journée";
                 break;
             case "Journee":
                 document.getElementById("dateHeure").style.display = 'none';
@@ -33,6 +35,7 @@
                 document.getElementById("dateJournee").style.display = 'block';
                 document.getElementById("dateSemaine").style.display = 'none';
                 document.getElementById("dateMois").style.display = 'none';
+                document.getElementById("durationSelected").innerHTML = "Journée";
                 break;
             case "Semaine":
                 document.getElementById("dateHeure").style.display = 'none';
@@ -40,6 +43,7 @@
                 document.getElementById("dateJournee").style.display = 'none';
                 document.getElementById("dateSemaine").style.display = 'block';
                 document.getElementById("dateMois").style.display = 'none';
+                document.getElementById("durationSelected").innerHTML = "Semaine";
                 break;
             case "Mois":
                 document.getElementById("dateHeure").style.display = 'none';
@@ -47,8 +51,24 @@
                 document.getElementById("dateJournee").style.display = 'none';
                 document.getElementById("dateSemaine").style.display = 'none';
                 document.getElementById("dateMois").style.display = 'block';
+                document.getElementById("durationSelected").innerHTML = "Mois";
                 break;                    
         }
+    }
+
+    function radioDemiJourneeClick(radioDuree){
+    	document.getElementById("durationDetailSelected").innerHTML = radioDuree.value;
+    }
+
+    function heureClick(duree){
+    	document.getElementById("durationDetailSelected").innerHTML = 'De ';
+    	document.getElementById("durationDetailSelected").innerHTML += document.getElementById("heure_start").value;
+    	document.getElementById("durationDetailSelected").innerHTML += ':';
+    	document.getElementById("durationDetailSelected").innerHTML += document.getElementById("min_start").value;
+    	document.getElementById("durationDetailSelected").innerHTML += ' à ';
+    	document.getElementById("durationDetailSelected").innerHTML += document.getElementById("heure_end").value;
+    	document.getElementById("durationDetailSelected").innerHTML += ':';
+    	document.getElementById("durationDetailSelected").innerHTML += document.getElementById("min_end").value;
     }
 
 jQuery(function($){
@@ -76,3 +96,4 @@ jQuery(function($){
 	});
 
 });
+
