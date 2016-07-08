690d0a4c2f02513e2637659a071e01996f5e4654
Olivia <odecharette@gmail.com>
2016-07-08 09:48:48 +0200

    Revert "test"
    This reverts commit f01e431849646353fdfc207a48eb127088d31fb5.

--- src/Becowo/MemberBundle/Controller/ConnectController.php
+++ src/Becowo/MemberBundle/Controller/ConnectController.php
@@ -17,13 +17,12 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Exception\AccountStatusException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\Security\Http\SecurityEvents;
 
-class ConnectController extends Symfony\Component\DependencyInjection\ContainerAware
+class ConnectController extends ContainerAware
 {
     public function registrationAction(Request $request, $key)
     {
