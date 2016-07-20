128b08fd0b663b4544bdabeb9e628ad4a9a2a9d0
Olivia <odecharette@gmail.com>
2016-07-20 11:37:21 +0200

    formulaire declarer Espace

--- src/Becowo/CoreBundle/Controller/HeaderController.php
+++ src/Becowo/CoreBundle/Controller/HeaderController.php
@@ -4,13 +4,53 @@ namespace Becowo\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class HeaderController extends Controller
 {
   
-  public function DeclarerEspaceAction()
+  public function DeclarerEspaceAction(Request $request)
   {
-    return $this->render('Footer/declarer-espace.html.twig');
+
+    $defaultData = array('description' => '');
+    $form = $this->createFormBuilder($defaultData)
+        ->add('email', EmailType::class, array('label' => 'Votre email *', 'required' => true))
+        ->add('nomEspace', TextType::class, array('label' => 'Le nom de l\'espace'))
+        ->add('website', TextType::class, array('label' => 'Site web'))
+        ->add('description', TextareaType::class, array('label' => 'Racontez-nous votre expérience *', 'required' => true))
+        ->add('Envoyer', SubmitType::class)
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+       $data = $form->getData();
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Un coworker déclare un nouvel espace')
+                ->setFrom($data['email'])
+                ->setTo('webmaster@becowo.com')
+                ->setBody(
+                    $this->renderView(
+                        'CommonViews/Mail/declarer-espace.html.twig',
+                        array(
+                            'email' => $data['email'],
+                            'nomEspace' => $data['nomEspace'],
+                            'website' => $data['website'],
+                            'description' => $data['description']
+                        )
+                    )
+                );
+
+            $this->get('mailer')->send($message);
+
+            $request->getSession()->getFlashBag()->add('success', 'Votre message a bien été envoyé');
+
+            return $this->redirectToRoute('becowo_core_declare_ws');
+    }
+
+    return $this->render('Header/declarer-espace.html.twig', array('form' => $form->createView()));
   }
 }
