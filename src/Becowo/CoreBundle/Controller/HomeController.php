e63ea5a076da284a3a697f24f2a0751ca3aa639f
Olivia <odecharette@gmail.com>
2016-08-11 13:56:38 +0200

    ordre body et script OK

--- src/Becowo/CoreBundle/Controller/HomeController.php
+++ src/Becowo/CoreBundle/Controller/HomeController.php
@@ -17,16 +17,23 @@ class HomeController extends Controller
     $WsService = $this->get('app.member');
     $members = $WsService->getLastActiveMembers(5);
    
-    $WsService = $this->get('app.map');
-    $mapGeoJson = $WsService->getWsAndPoiInGeoJson($workspaces);
+    // $WsService = $this->get('app.map');
+    // $mapGeoJson = $WsService->getWsAndPoiInGeoJson($workspaces);
+
+    //////////////// TO DO IMPORTANT
+    // Pour le moment, le XML de la map est généré dans MapController
+    // Désactiver le service map, la variable mapGeoJson
+    // Trouver un moyen de générer le XML directement (pour le moment, je lance le controller, récupère le contenu du dump et colle dans locations.xml)
+    // delete route extract et map
 
   	return $this->render('Home/home.html.twig', array(
   		'workspaces' => $workspaces, 
   		'members' => $members, 
   		'workspaceFavorite' => $workspaceFavorite,
       'picturesByWs' => $picturesByWs,
-      'officesByWS' => $officesByWS,
-      'mapGeoJson' => $mapGeoJson));
+      'officesByWS' => $officesByWS
+      // ,      'mapGeoJson' => $mapGeoJson
+      ));
   }
 
 }
