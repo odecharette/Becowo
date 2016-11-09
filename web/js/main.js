ce06ecbc2a39ae10a4567321cfa131399e8d69a6
Olivia <odecharette@gmail.com>
2016-11-09 16:59:09 +0100

    design

--- web/js/main.js
+++ web/js/main.js
@@ -134,27 +134,12 @@ $(function(){
 
 /***************** Booking steps ****************************/
 //http://www.jquery-steps.com/Examples#basic
-var form = $("#form-booking");
 
-form.$("#booking-steps").steps({
+$("#booking-steps").steps({
     headerTag: "titre",
     bodyTag: "section",
     transitionEffect: "slideLeft",
-    autoFocus: true,
-    onStepChanging: function (event, currentIndex, newIndex)
-    {
-        form.validate().settings.ignore = ":disabled,:hidden";
-        return form.valid();
-    },
-    onFinishing: function (event, currentIndex)
-    {
-        form.validate().settings.ignore = ":disabled";
-        return form.valid();
-    },
-    onFinished: function (event, currentIndex)
-    {
-        alert("Submitted!");
-    }
+    autoFocus: true
 });
 
 
