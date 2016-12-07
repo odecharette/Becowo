f89519aa18d8662cb61370ead5dbf8890a20b83c
Olivia <odecharette@gmail.com>
2016-12-07 17:20:26 +0100

    gros design page offres

--- web/js/main.js
+++ web/js/main.js
@@ -9,6 +9,18 @@
 
 // End Google Analytics 
 
+// Gestion de la class active sur la navbar
+$(document).ready(function () {
+    var url = window.location;
+    // Will only work if string in href matches with location
+    $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+
+    // Will also work for relative and absolute hrefs
+    $('ul.nav a').filter(function () {
+        return this.href == url;
+    }).parent().addClass('active').parent().parent().addClass('active');
+});
+
 // Charge la mini carte google dans la page d'un WS, selon son adresse dynamique
 var q=encodeURIComponent($('#address_ws').text());
        $('#map_ws')
