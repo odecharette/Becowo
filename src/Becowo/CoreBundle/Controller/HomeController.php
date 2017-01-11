10c30195888dc10008911418422d41f7ffff62e2
Olivia <odecharette@gmail.com>
2017-01-11 10:15:00 +0100

    debug

--- src/Becowo/CoreBundle/Controller/HomeController.php
+++ src/Becowo/CoreBundle/Controller/HomeController.php
@@ -8,6 +8,7 @@ class HomeController extends Controller
 {
   public function homeAction()
   {
+    /*
     $WsService = $this->get('app.workspace');
     $workspaces = $WsService->getActiveWorkspaces(); 
     $picturesByWs = $WsService->getPicturesByWorkspaces($workspaces);
@@ -16,13 +17,12 @@ class HomeController extends Controller
 
   	return $this->render('Home/home.html.twig', array(
   		'workspaces' => $workspaces, 
-  		//'members' => $members, 
   		'workspaceFavorite' => $workspaceFavorite,
       'picturesByWs' => $picturesByWs,
       'officesByWS' => $officesByWS
-      // 'comments' => $comments
-      // ,      'mapGeoJson' => $mapGeoJson
       ));
+    */
+      return $this->render('Home/home.html.twig');
   }
 
   public function communityAction()
