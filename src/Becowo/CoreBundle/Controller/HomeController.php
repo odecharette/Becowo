eff9381ac5940cb7298612ea80967db3a508952d
Olivia <odecharette@gmail.com>
2017-01-11 11:54:20 +0100

    mobile list ws on home page

--- src/Becowo/CoreBundle/Controller/HomeController.php
+++ src/Becowo/CoreBundle/Controller/HomeController.php
@@ -8,21 +8,37 @@ class HomeController extends Controller
 {
   public function homeAction()
   {
-    /*
-    $WsService = $this->get('app.workspace');
-    $workspaces = $WsService->getActiveWorkspaces(); 
-    $picturesByWs = $WsService->getPicturesByWorkspaces($workspaces);
-    $officesByWS = $WsService->getOfficesByWorkspaces($workspaces);
-    $workspaceFavorite = $WsService->getFavoriteWorkspace();
+    return $this->render('Home/home.html.twig');
+  }
 
-  	return $this->render('Home/home.html.twig', array(
-  		'workspaces' => $workspaces, 
-  		'workspaceFavorite' => $workspaceFavorite,
-      'picturesByWs' => $picturesByWs,
-      'officesByWS' => $officesByWS
-      ));
-    */
-      return $this->render('Home/home.html.twig');
+  public function mobileAction()
+  {
+    // This view in called only on mobiles
+  
+    $WsService = $this->get('app.workspace');
+    $WswithVote = $WsService->getActiveWorkspacesOrderByVoteAvg(); 
+    $AllActiveWs = $WsService->getActiveWorkspaces();
+
+    // En queryBuilder impossible de faire une requet en Union donc on récup 
+    // 1/ Tous les WS qui ont un vote, trié sur socreAVg en Desc
+    // 2/ Tous les WS
+    // 3/ On merge les 2 résultats
+    // 4/ On enlève les doublons
+    $workspaces = array_merge($WswithVote, $AllActiveWs);
+    $workspaces = array_unique($workspaces);
+
+    $wsFullInfo = array();
+    foreach ($workspaces as $w)
+    {
+      array_push($wsFullInfo, array('ws' => $w,
+        'lowestPrice' => $WsService->getLowestPriceByWorkspace($w),
+        'amenities' => $WsService->getAmenitiesByWorkspace($w),
+        'favoritePicture' => $WsService->getFavoritePictureUrlByWorkspace($w->getName()),
+        'averageVote' => round($WsService->getAverageVoteByWorkspace($w), 0),
+        'WsHasOffers' => $WsService->getOffersByWorkspace($w)) );
+    }
+
+    return $this->render('Home/ws-list-mobile.html.twig', array('wsFullInfo' => $wsFullInfo));
   }
 
   public function communityAction()
