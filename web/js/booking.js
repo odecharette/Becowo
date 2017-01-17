681d4dc63189b18b2bcd1e861bebed60ced94a87
Olivia <odecharette@gmail.com>
2017-01-17 18:00:00 +0100

    TICKET#200

--- web/js/booking.js
+++ web/js/booking.js
@@ -217,7 +217,21 @@ function remplirMinMaxTimeSlider(valeurs){
 
 function precise_round(num, decimals) {
    var t = Math.pow(10, decimals);   
-   return (Math.round((num * t) + (decimals>0?1:0)*(Math.sign(num) * (10 / Math.pow(100, decimals)))) / t).toFixed(decimals);
+   return (Math.round((num * t) + (decimals>0?1:0)*(mySign(num) * (10 / Math.pow(100, decimals)))) / t).toFixed(decimals);
+}
+
+function mySign(num) {
+	// IE does not support method sign here
+	if (typeof Math.sign === 'undefined') {
+		if (num > 0) {
+			return 1;
+		}
+		if (num < 0) {
+			return -1;
+		}
+		return 0;
+	}
+	return Math.sign(num);
 }
 
 function loadRecapDate(duree, value)
