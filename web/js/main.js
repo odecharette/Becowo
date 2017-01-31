887033f21a15b7e9d9c7e1d6e24bd20ecbd1791c
Olivia <odecharette@gmail.com>
2017-01-31 17:45:18 +0100

    design add FB

--- web/js/main.js
+++ web/js/main.js
@@ -233,6 +233,15 @@ $(document).ready(function(){
    
 });
 
+// Plugin FB like 
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.8&appId=810073549123890";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 }
 else
