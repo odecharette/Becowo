20c12732ea900e2666a857e5dfda842ae6507321
Olivia <odecharette@gmail.com>
2017-02-17 09:46:30 +0100

    page bills

--- src/Becowo/ManagerBundle/Controller/BillsController.php
+++ src/Becowo/ManagerBundle/Controller/BillsController.php
@@ -3,12 +3,21 @@
 namespace Becowo\ManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Finder\Finder;
 
 class BillsController extends Controller
 {
-  public function viewAction()
+  public function viewAction(Request $request)
   {
-  	return $this->render('Manager/bills.html.twig');
+  	$wsName = $this->getUser()->getWorkspace()->getName();
+	$finder = new Finder();
+	$dir = $this->container->getParameter('kernel.root_dir');
+	$finder->files()->in($dir.'/../web/documents/bills/'.$wsName.'/');
+	$finder->sortByName();
+	$finder->files()->name('*.pdf');
+
+  	return $this->render('Manager/bills.html.twig', array('finder' => $finder));
   }
 
 
