b82e2291621f21616778019c3b89396dc2a1fd5a
Olivia <odecharette@gmail.com>
2017-02-17 09:49:16 +0100

    page contrats

--- src/Becowo/ManagerBundle/Controller/ContractsController.php
+++ src/Becowo/ManagerBundle/Controller/ContractsController.php
@@ -3,12 +3,20 @@
 namespace Becowo\ManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Finder\Finder;
 
 class ContractsController extends Controller
 {
   public function viewAction()
   {
-  	return $this->render('Manager/contracts.html.twig');
+  	$wsName = $this->getUser()->getWorkspace()->getName();
+	$finder = new Finder();
+	$dir = $this->container->getParameter('kernel.root_dir');
+	$finder->files()->in($dir.'/../web/documents/contracts/'.$wsName.'/');
+	$finder->sortByName();
+	$finder->files()->name('*.pdf');
+
+  	return $this->render('Manager/contracts.html.twig', array('finder' => $finder));
   }
 
 
