81b7adaab6f98452e6fd8409e3c448642eea090e
Olivia <odecharette@gmail.com>
2017-03-08 17:08:11 +0100

    update cron script

--- web/Batch/EmailNewUsers.sh
+++ web/Batch/EmailNewUsers.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/usr/local/php5.6/bin/php /homez.2332/coworkinwq/bin/console app:send-email-new-users --env=demo > /homez.2332/coworkinwq/./demo/Becowo/var/logs/Cron/envoieEmail.txt
+today=$(date +"%Y-%m-%d-%H")
+/usr/local/php5.6/bin/php /homez.2332/coworkinwq/./demo/Becowo/bin/console app:send-email-new-users --env=demo > /homez.2332/coworkinwq/./demo/Becowo/var/logs/Cron/emailNewUsers-$today.txt
