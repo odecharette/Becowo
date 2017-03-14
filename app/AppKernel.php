5ae38e11fd06a7f657d350ef1dbc9af3a574b860
Olivia <odecharette@gmail.com>
2017-03-14 17:48:50 +0100

    try to get toolbar in demo / not showing
    il faut call demo.becowo.com/app_dev.php pour la voir

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,7 +34,7 @@ class AppKernel extends Kernel
             new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), ['dev', 'demo'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
