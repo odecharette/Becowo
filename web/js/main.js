5a1d67dc04ba375708a4ab1803337d489159bd36
Olivia <odecharette@gmail.com>
2017-03-15 14:49:06 +0100

    change google analytics ID by env

--- web/js/main.js
+++ web/js/main.js
@@ -4,7 +4,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-77791149-2', 'auto');
+  ga('create', $('#google_analytics_ID').text(), 'auto');
   ga('send', 'pageview');
 
 // End Google Analytics 
