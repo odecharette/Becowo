af9fb04e036d01a61bd75395052a7cb97a7770ca
Olivia <odecharette@gmail.com>
2017-03-16 16:48:22 +0100

    correct lowest price label

--- src/Becowo/CoreBundle/EventListener/PriceListener.php
+++ src/Becowo/CoreBundle/EventListener/PriceListener.php
@@ -36,21 +36,23 @@ class PriceListener
         {
             //
         }else{
-            $a = array('heure' => $p->getPriceHour(), 
-                        '1/2 journée' => $p->getPriceHalfDay(),
-                        'jour' => $p->getPriceDay(),
-                        'mois' => $p->getPriceMonth());
+            // on round le prix, sinon ca pose problème pour min() qui exclue les valeurs NULL
+            $a = array('heure' => intval($p->getPriceHour()), 
+                        '1/2 journée' => intval($p->getPriceHalfDay()),
+                        'jour' => intval($p->getPriceDay()),
+                        'mois' => intval($p->getPriceMonth()));
            
             // renvoi le plus petit prix en excluant les valeurs NULL tout en gardant le nom de la Key
             $minPriceLabel = array_search(min(array_diff(array_map('intval', $a), array(0))), $a);
             $minPrice = min(array_diff(array_map('intval', $a), array(0)));
- 
-        $allWsMinPrices[$minPriceLabel] = $minPrice;
+
+        array_push($allWsMinPrices, [$minPriceLabel, $minPrice]);
         }
 
     }
+
     if(count($allWsMinPrices) > 0)
-      $lowestPrice = round(min($allWsMinPrices)) . '<sup>€</sup> / ' . array_search(min($allWsMinPrices), $allWsMinPrices);
+      $lowestPrice = round(min(array_column($allWsMinPrices, 1))) . '<sup>€</sup> / ' . $allWsMinPrices[array_search(min(array_column($allWsMinPrices, 1)), $allWsMinPrices)][0];
     else
       $lowestPrice = 0;
 
