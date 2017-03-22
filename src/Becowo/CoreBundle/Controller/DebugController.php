a209b0a86c124fc6a45ff880917e21de22e94a61
Olivia <odecharette@gmail.com>
2017-03-22 15:56:12 +0100

    create debug route and controller

---
+++ src/Becowo/CoreBundle/Controller/DebugController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Becowo\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DebugController extends Controller
+{
+
+  public function viewAction()
+  {
+    $contentToDump = "";
+    $content = "";
+
+    $contentToDump = $this->get('mailer')->getTransport()->getSpool();
+
+    return $this->render('Debug/view.html.twig', array('content' => $content, 'contentToDump' => $contentToDump));
+
+  }
+
+
+}
