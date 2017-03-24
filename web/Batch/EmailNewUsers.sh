46e87032073f2300794364acc97ddfe241e2e9ef
Olivia <odecharette@gmail.com>
2017-03-24 17:38:35 +0100

    batch cron en PHP 7

--- web/Batch/EmailNewUsers.sh
+++ web/Batch/EmailNewUsers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 today=$(date +"%Y-%m-%d-%H")
-/usr/local/php5.6/bin/php /homez.2332/coworkinwq/./demo/Becowo/bin/console app:send-email-new-users --env=demo > /homez.2332/coworkinwq/./demo/Becowo/var/logs/Cron/emailNewUsers-$today.txt
+/usr/local/php7.0/bin/php /homez.2332/coworkinwq/./demo/Becowo/bin/console app:send-email-new-users --env=demo > /homez.2332/coworkinwq/./demo/Becowo/var/logs/Cron/emailNewUsers-$today.txt
