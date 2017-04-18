16a839cb8117bc84f040fc6693f8670f35f659ec
Olivia <odecharette@gmail.com>
2017-04-18 11:44:41 +0200

    debug prod if blog catch exception

--- src/Becowo/CoreBundle/Controller/BlogController.php
+++ src/Becowo/CoreBundle/Controller/BlogController.php
@@ -11,26 +11,28 @@ class BlogController extends Controller
   {
     $urlBlogRSS = $this->container->getParameter('blogRSS');
 
+    $articles = array();
     try {
       $content = file_get_contents($urlBlogRSS);
-    } catch (Exception $e) {
-      return $this->render('Home/blog.html.twig', array('articles' => ""));
+      $node = new \SimpleXmlElement($content);
+       
+
+      $i = 0;
+      foreach($node->channel->item as $entry) {
+        $articles[$i]['titre'] = $entry->title;
+        $articles[$i]['url'] = $entry->link;
+        $articles[$i]['description'] = $entry->description;
+        $i ++;
+        if($i == $nbArticles){
+          break;
+        }
+      }
+    } catch (\Exception $e) {
+      $articles[0]['titre'] = "";
+      $articles[0]['url'] = "";
+      $articles[0]['description'] = "Le contenu du blog est momentanément indisponible. <br> Veuillez recharger la page";
     }
     
-    $node = new \SimpleXmlElement($content);
-     
-    $articles = array();
-
-    $i = 0;
-    foreach($node->channel->item as $entry) {
-      $articles[$i]['titre'] = $entry->title;
-      $articles[$i]['url'] = $entry->link;
-      $articles[$i]['description'] = $entry->description;
-      $i ++;
-      if($i == $nbArticles){
-        break;
-      }
-    }
 
   return $this->render('Home/blog.html.twig', array('articles' => $articles));
 
